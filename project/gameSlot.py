47af5c890e0fec044f08df82b02172ab1898be76
Minori Olguin <minoriiolguin@gmail.com>
2024-12-03 18:16:06 -0700

    added is full functions to practice slot and game slots

--- project/gameSlot.py
+++ project/gameSlot.py
@@ -6,7 +6,10 @@ class GameSlot:
         self.startTime = line[1].strip()
         self.gameMax = int(line[2].strip())
         self.gameMin = int(line[3].strip())
-        # self.assignGames = []
+        self.assignGames = []
+        
+    def is_full(self):
+        return len(self.assignGames) >= self.gameMax
 
     def __repr__(self):
         return f"GameSlot(day={self.day}, startTime={self.startTime}, gameMax={self.gameMax}, gameMin={self.gameMin})"
--- project/practiceSlot.py
+++ project/practiceSlot.py
@@ -6,7 +6,10 @@ class PracticeSlot:
         self.startTime = line[1].strip()
         self.pracMax = int(line[2].strip())
         self.pracMin = int(line[3].strip())
-        # self.assignPractices = []
-
+        self.assignPractices = []
+        
+    def is_full(self):
+        return len(self.assignPractices) >= self.pracMax
+    
     def __repr__(self):
         return f"PracticeSlot(day={self.day}, startTime={self.startTime}, pracMax={self.pracMax}, pracMin={self.pracMin})"
